2aae9931010dcc4edb592886e33b5488280df7bf
baripembo <erikawei@gmail.com>
2025-01-28 12:04:41 -0500

    updating basepath

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hdx-hapi-sudan/',
+  base: '/hdx-hapi-southsudan/',
   plugins: [
     svelte()
   ],
